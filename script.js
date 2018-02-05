3747f2b793cd964bf35e8cf416152e4a05a56fc0
antsuma <anton.nordgren1@gmail.com>
2018-02-05 11:47:12 +0100

    asd

--- script.js
+++ script.js
@@ -15,8 +15,6 @@ const db = firebase.database();
 let content = document.getElementById("loggedInContent");
 let avatar = document.getElementById("avatar");
 
-//avatar.src = "https://avatars3.githubusercontent.com/u/32438821?v=4";
-
 let provider = new firebase.auth.GithubAuthProvider();
 provider.setCustomParameters({
     'allow_signup' : 'true'
@@ -25,8 +23,8 @@ provider.setCustomParameters({
 document.getElementById("githubButton").addEventListener("click", function(){
     firebase.auth().signInWithPopup(provider).then(function(result) {
         console.log(result);
-        //content.innerHTML = "Username: " + result.additionalUserInfo.username + "<br>";
-        //avatar.src = result.additionalUserInfo.profile.avatar_url;
+        content.innerHTML = "Username: " + result.additionalUserInfo.username + "<br>";
+        avatar.src = result.additionalUserInfo.profile.avatar_url;
     }).catch(function(error) {
         console.log("Popup result, error: " + error.message);
     });
